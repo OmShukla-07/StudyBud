8794b74c031cb3b8ee939259f41cceda81804ea3
OmShukla-07 <omshukla2025@gmail.com>
2025-07-22 23:24:33 +0530

    some more advancements

--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id' : 1, 'name': 'I am Ironman'},
-    {'id' : 2, 'name': 'I am Spiderman'},
-    {'id' : 3, 'name': 'I am Batman'},
-]
+##rooms = [
+##    {'id' : 1, 'name': 'I am Ironman'},
+##    {'id' : 2, 'name': 'I am Spiderman'},
+##    {'id' : 3, 'name': 'I am Batman'},
+##]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id= pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
